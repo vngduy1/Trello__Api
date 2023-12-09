3dfc7fface144b49035ad3624d3f476a7ed629ef
Duy <duyvu372001@gmail.com>
2023-12-10 00:14:49 +0900

    Add library babel

--- src/server.js
+++ src/server.js
@@ -1,14 +1,36 @@
-const express = require("express");
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
+import express from "express";
+import { mapOrder } from "~/utils/sorts.js";
 
 const app = express();
 
 const hostname = "localhost";
-const post = 8017;
+const port = 8017;
 
 app.get("/", (req, res) => {
-  res.send("helloworld");
+  // Test Absolute import mapOrder
+  console.log(
+    mapOrder(
+      [
+        { id: "id-1", name: "One" },
+        { id: "id-2", name: "Two" },
+        { id: "id-3", name: "Three" },
+        { id: "id-4", name: "Four" },
+        { id: "id-5", name: "Five" },
+      ],
+      ["id-5", "id-4", "id-2", "id-3", "id-1"],
+      "id"
+    )
+  );
+  res.end("<h1>Hello World!</h1><hr>");
 });
 
-app.listen(post, hostname, () => {
-  console.log(`hello Duy, running sever at ${hostname}:${post}`);
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello duy, I am running at ${hostname}:${port}/`);
 });
